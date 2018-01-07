af40093e09d0e6e9db2988b4a42ee0abd2687068
zuoguangcheng <zuoguangcheng@shuidao.com>
2018-01-07 23:40:16 +0800

    需改进入聊天室逻辑， 添加mongodb

--- src/room/model.js
+++ src/room/model.js
@@ -1,6 +1,6 @@
 const _default = {
   maxNum: 100,
-}
+};
 
 class RoomInfo {
   constructor(id, name, user) {
@@ -9,9 +9,11 @@ class RoomInfo {
     this.persons = [];
     this.maxNum = _default.maxNum;
   }
+
   setPersons(user) {
     this.persons.push(user);
   }
+
   leavePersons(user) {
     const index = this.persons.indexOf(user);
     this.persons.splice(index, 1);
--- src/user/index.js
+++ src/user/index.js
@@ -1,5 +1,13 @@
 let rooms = require('../room');
 
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/socket';
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbase = db.db('socket');
+  var rooms = dbase.collection('rooms');
+});
+
 class UserInfo {
   constructor(id, name, socket) {
     this.name = name;
@@ -9,32 +17,55 @@ class UserInfo {
   }
 
   init() {
-    this.socket.emit('rooms', new rooms().getRooms());
-    this.socket.on('chatMsg', (id, record) => {
-      console.log('id', id)
-      console.log('record', record)
-      this.socket.to(id).emit('chatMsg', record)
-    })
+    this.getRoomsList();
+    this.getChatMsg();
+
     this.getChannelDetail();
+    this.leaveRooms();
+    this.selectRooms();
+  }
+
+  getChatMsg() {
+    this.socket.on('chatMsg', (id, record) => {
+      this.socket.to(id).emit('chatMsg', record);
+    });
+  }
+
+  getRoomsList() {
+    this.socket.emit('rooms', new rooms().getRooms());
+  }
+
+  leaveRooms() {
+    this.socket.on('leaveRooms', id => {
+      console.log('leaveRooms', id);
+    });
+
+    // 清除之前加入过的房间
+    let roomss = Object.keys(this.socket.rooms);
+    let leaveRooms = roomss.splice(1, roomss.length);
+    if (leaveRooms.length > 0) {
+      leaveRooms.forEach(element => {
+        this.socket.leave(element);
+      });
+    }
+  }
+
+  selectRooms() {
+    this.socket.on('selectRooms', id => {
+      console.log('selectRooms', id);
+      this.getChannelDetail();
+    });
   }
 
   getChannelDetail() {
     this.socket.on('getChannel', id => {
-      // 清除之前加入过的房间
-      let roomss = Object.keys(this.socket.rooms);
-      let leaveRooms = roomss.splice(1, roomss.length);
-      if (leaveRooms.length > 0) {
-        leaveRooms.forEach(element => {
-          this.socket.leave(element);
-        });
-      }
 
       this.socket.join(id, (e) => {
         const roomsList = new rooms().getRooms();
         let roomDetail = roomsList.filter(item => {
-          return item.id === id
-        })
-        this.socket.emit('roomsDetail', roomDetail)
+          return item.id === id;
+        });
+        this.socket.emit('roomsDetail', roomDetail);
       });
     });
   }
